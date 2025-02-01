dd2d59231d6e268d920ee1d63894a370505eb96d
Yibo Wang <89687788+yibowang2004@users.noreply.github.com>
2025-02-01 16:00:04 +0800

    Update moreClick.js

--- scripts/moreClick.js
+++ scripts/moreClick.js
@@ -7,32 +7,50 @@ more.addEventListener('click', function () {
         container.style.left = container.style.left === '50%' ? '57.5%' : '50%';
         container.style.width = container.style.width === '80%' ? '60%' : '80%';
     }
+    if(window.innerWidth >= 600) {
+        leftbar.style.width = '360px';
+    }
+    if(window.innerWidth < 600) {
+        leftbar.style.width = '60%';
+    }
 });
 
 window.onresize = function () {
     let viewportWidth = window.innerWidth;
-    if(viewportWidth >= 1400) {
+    if(viewportWidth > 1400) {
         leftbar.style.display = 'block';
         container.style.left = '57.5%';
         container.style.width = '60%';
     }
-    if(viewportWidth < 1400) {
+    if(viewportWidth <= 1400) {
         leftbar.style.display = 'none';
         container.style.left = '50%';
         container.style.width = '80%';
+    }
+    if(window.innerWidth >= 600) {
+        leftbar.style.width = '360px';
+    }
+    if(window.innerWidth < 600) {
+        leftbar.style.width = '60%';
     }
 }
 
 window.onload = function() {
     let viewportWidth = window.innerWidth;
-    if(viewportWidth >= 1400) {
+    if(viewportWidth > 1400) {
         leftbar.style.display = 'block';
         container.style.left = '57.5%';
         container.style.width = '60%';
     }
-    if(viewportWidth < 1400) {
+    if(viewportWidth <= 1400) {
         leftbar.style.display = 'none';
         container.style.left = '50%';
         container.style.width = '80%';
+    }
+    if(window.innerWidth >= 600) {
+        leftbar.style.width = '360px';
+    }
+    if(window.innerWidth < 600) {
+        leftbar.style.width = '60%';
     }
 };
