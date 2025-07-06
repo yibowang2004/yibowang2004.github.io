65dd8126b2e9cf62f8fc879d47c5806595328eab
Yibo Wang <89687788+yibowang2004@users.noreply.github.com>
2025-07-06 12:27:26 +0100

    Add files via upload

--- scripts/leftbar.js
+++ scripts/leftbar.js
@@ -1,15 +1,16 @@
 const more = document.querySelector('.header .more');
+const wrapper = document.querySelector('.wrapper');
 const leftbar = document.querySelector('.leftbar');
 const container = document.querySelector('.container');
 const threshold = 1400;
 more.addEventListener('click', function () {
     leftbar.style.display = leftbar.style.display === 'none' ? 'block' : 'none';
     if(window.innerWidth > threshold) {
-        container.style.left = container.style.left === '50%' ? '57.5%' : '50%';
-        container.style.width = container.style.width === '90%' ? '60%' : '90%';
+        container.style.left = container.style.left === '50%' ? '62.5%' : '50%';
+        container.style.width = container.style.width === '90%' ? '65%' : '90%';
     }
     if(window.innerWidth >= 515) {
-        leftbar.style.width = '360px';
+        leftbar.style.width = '320px';
     }
     if(window.innerWidth < 515) {
         leftbar.style.width = '70%';
@@ -19,19 +20,21 @@ more.addEventListener('click', function () {
 window.onresize = function () {
     let viewportWidth = window.innerWidth;
     if(viewportWidth > threshold) {
+        wrapper.style.width = '80%';
         leftbar.style.display = 'block';
         leftbar.dataset.w = viewportWidth;
-        container.style.left = '57.5%';
-        container.style.width = '60%';
+        container.style.left = '62.5%';
+        container.style.width = '65%';
     }
     if(viewportWidth <= threshold && leftbar.dataset.w > threshold) {
+        wrapper.style.width = '100%';
         leftbar.style.display = 'none';
         leftbar.dataset.w = viewportWidth;
         container.style.left = '50%';
         container.style.width = '90%';
     }
     if(window.innerWidth >= 515) {
-        leftbar.style.width = '360px';
+        leftbar.style.width = '320px';
     }
     if(window.innerWidth < 515) {
         leftbar.style.width = '70%';
@@ -41,19 +44,21 @@ window.onresize = function () {
 window.onload = function() {
     let viewportWidth = window.innerWidth;
     if(viewportWidth > threshold) {
+        wrapper.style.width = '80%';
         leftbar.style.display = 'block';
         leftbar.dataset.w = viewportWidth;
-        container.style.left = '57.5%';
-        container.style.width = '60%';
+        container.style.left = '62.5%';
+        container.style.width = '65%';
     }
     if(viewportWidth <= threshold) {
+        wrapper.style.width = '100%';
         leftbar.style.display = 'none';
         leftbar.dataset.w = viewportWidth;
         container.style.left = '50%';
         container.style.width = '90%';
     }
     if(window.innerWidth >= 515) {
-        leftbar.style.width = '360px';
+        leftbar.style.width = '320px';
     }
     if(window.innerWidth < 515) {
         leftbar.style.width = '70%';
