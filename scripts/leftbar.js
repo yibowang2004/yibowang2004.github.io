d5423bce90ebda2597f3a86746b438e6f9adfe99
Yibo Wang <89687788+yibowang2004@users.noreply.github.com>
2025-07-26 23:54:03 +0100

    Add files via upload

--- scripts/leftbar.js
+++ scripts/leftbar.js
@@ -2,12 +2,13 @@ const more = document.querySelector('.header .more');
 const wrapper = document.querySelector('.wrapper');
 const leftbar = document.querySelector('.leftbar');
 const container = document.querySelector('.container');
+const clickbar = document.querySelector('.clickbar');
 const threshold = 1400;
 more.addEventListener('click', function () {
-    leftbar.style.display = leftbar.style.display === 'none' ? 'block' : 'none';
+    leftbar.style.display = leftbar.style.display === 'block' ? 'none' : 'block';
     if(window.innerWidth > threshold) {
-        container.style.left = container.style.left === '50%' ? '62.5%' : '50%';
-        container.style.width = container.style.width === '90%' ? '65%' : '90%';
+        container.style.left = container.style.left === '60%' ? '50%' : '60%';
+        container.style.width = container.style.width === '65%' ? '90%' : '65%';
     }
     if(window.innerWidth >= 515) {
         leftbar.style.width = '320px';
@@ -21,17 +22,17 @@ window.onresize = function () {
     let viewportWidth = window.innerWidth;
     if(viewportWidth > threshold) {
         wrapper.style.width = '80%';
-        leftbar.style.display = 'block';
+        // leftbar.style.display = 'block';
         leftbar.dataset.w = viewportWidth;
-        container.style.left = '62.5%';
-        container.style.width = '65%';
+        // container.style.left = '62.5%';
+        // container.style.width = '65%';
     }
     if(viewportWidth <= threshold && leftbar.dataset.w > threshold) {
         wrapper.style.width = '100%';
         leftbar.style.display = 'none';
         leftbar.dataset.w = viewportWidth;
-        container.style.left = '50%';
-        container.style.width = '90%';
+        // container.style.left = '50%';
+        // container.style.width = '90%';
     }
     if(window.innerWidth >= 515) {
         leftbar.style.width = '320px';
@@ -45,17 +46,17 @@ window.onload = function() {
     let viewportWidth = window.innerWidth;
     if(viewportWidth > threshold) {
         wrapper.style.width = '80%';
-        leftbar.style.display = 'block';
+        // leftbar.style.display = 'block';
         leftbar.dataset.w = viewportWidth;
-        container.style.left = '62.5%';
-        container.style.width = '65%';
+        // container.style.left = '62.5%';
+        // container.style.width = '65%';
     }
     if(viewportWidth <= threshold) {
         wrapper.style.width = '100%';
         leftbar.style.display = 'none';
         leftbar.dataset.w = viewportWidth;
-        container.style.left = '50%';
-        container.style.width = '90%';
+        // container.style.left = '50%';
+        // container.style.width = '90%';
     }
     if(window.innerWidth >= 515) {
         leftbar.style.width = '320px';
